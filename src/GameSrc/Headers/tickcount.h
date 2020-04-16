d694f11c51ad4cc8a42b8aa1112678cef75e64ed
Vladimir Olteanu <vladimir.olteanu@cs.pub.ro>
2020-04-16 19:45:55 +0300

    stuff

--- src/GameSrc/Headers/tickcount.h
+++ src/GameSrc/Headers/tickcount.h
@@ -25,6 +25,9 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // http://mirror.informatimago.com/next/developer.apple.com/documentation/mac/Toolbox/Toolbox-80.html
 // number of ticks since system start (1 Tick is about 1/60 second)
-uint32_t TickCount(void);
+inline uint32_t TickCount(void)
+{
+	return SDL_GetTicks() * 100 / 357; // 280 per second;
+}
 
 #endif // TICKCOUNT_H_
